d0431c2cb25f036003492afbf2b387d1808c60bc
Iqbal <dibaliqaja@gmail.com>
2021-01-12 22:24:39 +0700

    first commit

---
+++ client/pages/home.vue
@@ -0,0 +1,16 @@
+<template>
+  <card :title="$t('home')">
+    {{ $t('you_are_logged_in') }}
+    <router-link to="/profile">Lihat profil</router-link>
+  </card>
+</template>
+
+<script>
+export default {
+  middleware: 'auth',
+
+  head () {
+    return { title: this.$t('home') }
+  }
+}
+</script>
---
+++ client/pages/profile.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="p-5">
+            <h1>{{ user.name }}</h1>
+            <img :src="user.photo_url" alt="photo" style="width: 100px">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+
+  computed: mapGetters({
+    user: 'auth/user'
+  }),
+
+}
+</script>
